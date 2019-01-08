09e2db8e71dd4a4b64f19038c2ec7a8ec534ea80
Abhishek Gupta <grapho.dev@gmail.com>
2019-01-09 02:04:14 +0530

    Initial commit

---
+++ src/main/java/sample/HelloWorldApplication.java
@@ -0,0 +1,34 @@
+package sample;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import sample.resources.HelloWorldResource;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "HelloWorld";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final HelloWorldConfiguration configuration,
+                    final Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+    }
+
+}
